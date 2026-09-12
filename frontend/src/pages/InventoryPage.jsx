c7bce2c819f342b88bfe64f81726fc1ca11f0acb
Agencia IA <agencia@ia.com>
2026-09-12 10:31:17 -0500

    Change

--- frontend/src/pages/InventoryPage.jsx
+++ frontend/src/pages/InventoryPage.jsx
@@ -5,7 +5,7 @@ import Badge from '../components/ui/Badge';
 import { useInventoryStore } from '../stores/useInventoryStore';
 import { useCatalogStore } from '../stores/useCatalogStore';
 import StockEntryModal from '../components/inventory/StockEntryModal';
-import { formatCurrency } from '../utils/formatters';
+import { formatCurrency } from '../lib/formatters';
 
 const formatDecimal = (value) => {
   const n = Number(value);
